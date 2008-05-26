b2831bc8ef45cd3c62301d506e2945932d803840
Reinier Heeres <reinier@heeres.eu>
2008-05-26 13:04:24 +0200

    Update RS_SMR40

--- instrument_plugins/RS_SMR40.py
+++ instrument_plugins/RS_SMR40.py
@@ -1,6 +1,6 @@
 # RS_SMR40.py class, to perform the communication between the Wrapper and the device
 # Pieter de Groot <pieterdegroot@gmail.com>, 2008
-# Martijn Schaafsma <m.c.schaafsma@student.tudelft.nl>, 2008
+# Martijn Schaafsma <mcschaafsma@gmail.com>, 2008
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -23,33 +23,31 @@ import logging
 
 class RS_SMR40(Instrument):
     '''
-    This is the python driver for the Rohde & Schwarz SMR40 
+    This is the python driver for the Rohde & Schwarz SMR40
     signal generator
-    
+
     Usage:
     Initialize with
     <name> = instruments.create('name', 'RS_SMR40', address='<GPIB address>',
         reset=<bool>)
-        
-    Callable functions:
-    <name>.reset()
-    <name>.get_all()
-    <name>.get_frequency()
-    <name>.set_frequency(<value>)
-    <name>.get_power()
-    <name>.set_power(<value>)
-    <name>.get_status()
-    <name>.set_status(<'on' or 'off'>)   
-    
-    Shortcuts:
-    <name>.on()
-    <name>.off()
     '''
 
-    def __init__(self, name, address, reset=False): #  address als derde parameter verwijderd!!
-        Instrument.__init__(self, name)
+    def __init__(self, name, address, reset=False):
+        '''
+        Initializes the RS_SMR40, and communicates with the wrapper.
 
-        self._address = address        
+        Input:
+            name (string)    : name of the instrument
+            address (string) : GPIB address
+            reset (bool)     : resets to default values, default=false
+
+        Output:
+            None
+        '''
+        logging.info(__name__ + ' : Initializing instrument')
+        Instrument.__init__(self, name, tags=['physical'])
+
+        self._address = address
         self._visainstrument = visa.instrument(self._address)
 
         self.add_parameter('frequency', type=types.FloatType,
@@ -60,120 +58,158 @@ class RS_SMR40(Instrument):
             minval=-30, maxval=25, units='dBm')
         self.add_parameter('status', type=types.StringType,
             flags=Instrument.FLAG_GETSET | Instrument.FLAG_GET_AFTER_SET)
-            
-        self.add_function('reset')            
+
+        self.add_function('reset')
         self.add_function('get_all')
-            
+
         if reset:
             self.reset()
-        else: 
+        else:
             self.get_all()
-           
+
     # Functions
     def reset(self):
         '''
-        reset()
-        Reset device to default values
-        '''        
-        logging.debug(__name__ + ' : Resetting instrument')
+        Resets the instrument to default values
+
+        Input:
+            None
+
+        Output:
+            None
+        '''
+        logging.info(__name__ + ' : Resetting instrument')
         self._visainstrument.write('*RST')
-        
-        #This updates all the variables in memory to the instrument values
         self.get_all()
 
     def get_all(self):
         '''
-        get_all()
-        Read settings from device
-        '''        
-        logging.debug(__name__ + ' : reading all settings from instrument')
-        self._frequency = self.get_frequency()
-        self._power = self.get_power()
-        self._status = self.get_status()
+        Reads all implemented parameters from the instrument,
+        and updates the wrapper.
 
-# communication with machine
+        Input:
+            None
+
+        Output:
+            None
+        '''
+        logging.info(__name__ + ' : reading all settings from instrument')
+        self.get_frequency()
+        self.get_power()
+        self.get_status()
+
+    # communication with machine
 
     def _do_get_frequency(self):
         '''
-        _do_get_frequency()
         Get frequency from device
-        '''        
+
+        Input:
+            None
+
+        Output:
+            frequency (float) : frequency in Hz
+        '''
         logging.debug(__name__ + ' : reading frequency from instrument')
-        # getting value from instrument to memory
-        self._frequency = float(self._visainstrument.ask('SOUR:FREQ?'))
-        return self._frequency
+        return float(self._visainstrument.ask('SOUR:FREQ?'))
 
     def _do_set_frequency(self, frequency):
         '''
-        _do_set_frequency(frequency)
         Set frequency of device
-        '''	
+
+        Input:
+            frequency (float) : frequency in Hz
+
+        Output:
+            None
+        '''
         logging.debug(__name__ + ' : setting frequency to %s GHz' % frequency)
-        # sending value to instrument
         self._visainstrument.write('SOUR:FREQ %e' % frequency)
 
     def _do_get_power(self):
         '''
-        _do_get_power()
         Get output power from device
-        '''	
+
+        Input:
+            None
+
+        Output:
+            power (float) : output power in dBm
+        '''
         logging.debug(__name__ + ' : reading power from instrument')
-        # getting value from instrument to memory
-        self._power = float(self._visainstrument.ask('SOUR:POW?'))
-        return self._power
+        return float(self._visainstrument.ask('SOUR:POW?'))
 
     def _do_set_power(self,power):
         '''
-        _do_set_power(power)
         Set output power of device
-        '''	
+
+        Input:
+            power (float) : output power in dBm
+
+        Output:
+            None
+        '''
         logging.debug(__name__ + ' : setting power to %s dBm' % power)
-        # sending value to instrument
-        self._visainstrument.ask('SOUR:POW %e' % power) 
+        self._visainstrument.ask('SOUR:POW %e' % power)
 
     def _do_get_status(self):
         '''
-        _do_get_status()
-        Get status from device
-        '''	
+        Get status from instrument
+
+        Input:
+            None
+
+        Output:
+            status (string) : 'on or 'off'
+        '''
         logging.debug(__name__ + ' : reading status from instrument')
-        # getting value from instrument to memory
-        cmd = self._visainstrument.ask(':OUTP:STAT?')
-        
-        if cmd == '1':
-            self._status = 'on'
-        elif cmd == '0':
-            self._status = 'off'
+        stat = self._visainstrument.ask(':OUTP:STAT?')
+
+        if stat == '1':
+            return 'on'
+        elif stat == '0':
+            return 'off'
         else:
-            self._status = 'error'
-            logging.debug(__name__ + ' : Unexpected Readout from get_status ')            
-        
-        return self._status
- 
+            raise ValueError('Output status not specified : %s' % stat)
+
     def _do_set_status(self,status):
         '''
-        _do_set_status(status)
-        Set status to 'on' or 'off'
-        '''	
+        Set status of instrument
+
+        Input:
+            status (string) : 'on or 'off'
+
+        Output:
+            None
+        '''
+        logging.debug(__name__ + ' : setting status to "%s"' % status)
         if status.upper() in ('ON', 'OFF'):
             status = status.upper()
         else:
             raise ValueError('set_status(): can only set on or off')
-        logging.debug(__name__ + ' : setting status to "%s"' % status)
-        # sending value to instrument
-        self._visainstrument.write(':OUTP:STAT %s' % status) 
+        self._visainstrument.write(':OUTP:STAT %s' % status)
 
     # shortcuts
     def off(self):
         '''
-        off()
         Set status to 'off'
+
+        Input:
+            None
+
+        Output:
+            None
         '''
         self.set_status('off')
 
     def on(self):
         '''
-        on()
         Set status to 'on'
+
+        Input:
+            None
+
+        Output:
+            None
         '''
         self.set_status('on')
