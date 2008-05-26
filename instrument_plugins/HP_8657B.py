1ac7ad8c8f9502c9634b945e93815a2e38d5f7dd
Reinier Heeres <reinier@heeres.eu>
2008-05-26 13:01:27 +0200

    Update HP_8657B

--- instrument_plugins/HP_8657B.py
+++ instrument_plugins/HP_8657B.py
@@ -1,6 +1,6 @@
-# HP8657B.py class, to perform the communication between the Wrapper and the device
+# HP_8657B.py class, to perform the communication between the Wrapper and the device
 # Pieter de Groot <pieterdegroot@gmail.com>, 2008
-# Martijn Schaafsma <m.c.schaafsma@student.tudelft.nl>, 2008
+# Martijn Schaafsma <mcschaafsma@gmail.com>, 2008
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,51 +22,44 @@ import types
 import logging
 from time import sleep
 
-class HP8657B(Instrument):
+class HP_8657B(Instrument):
     '''
-    This is the python driver for the HP 8657B 
+    This is the python driver for the HP 8657B
     signal generator
-    
+
     Usage:
     Initialise with
-    <name> = instruments.create('<name>', 'HP8657B', address='<GPIB address>',
+    <name> = instruments.create('<name>', 'HP_8657B', address='<GPIB address>',
         reset=<bool>, freq=<float>, pow=<float>)
-        
+
     The last three parameters are optional. Default parameters are
     reset=False, freq=1e9, pow=-143.4
-    
-    Note that readout of device is not implemented
-    
-    Callable functions:
-    <name>.set_frequency(<float>)
-    <name>.set_power(<float>)
-    <name>.set_status(<'on' or 'off'>)
-    <name>.reset(freq=<float>, pow=<float>)
-    <name>.set_all(freq=<float>, pow=<float>)
-    <name>.on()
-    <name>.off()
 
-    
+    Note that readout of device is not implemented
+
     TODO:
-    1. Implement a reset that DOES work
-    2. Adjust timing
+    2. Adjust timing/sleep
+    3. Units
     '''
 
     def __init__(self, name, address, reset=False, freq=1e9, pow=-143.4):
         '''
-        Initializes the driver
-        __init__(self, name, address, reset=False, freq=1e9, pow=-143.4):
-        
-        Loads the following functions into the wrapper
-            <name>.set_frequency(<float>)
-            <name>.set_power(<float>)
-            <name>.set_status(<'on' or 'off'>)
-            <name>.reset(freq=<float>, pow=<float>)
-            <name>.set_all(freq=<float>, pow=<float>)
-        '''
-        Instrument.__init__(self, name)
+        Initializes the HP_8657B, and communicates with the wrapper.
+        This instrument does not support GPIB talking, so 'get' functions
+        are not available.
+        Only use with reset=True, otherwise warning will be given.
 
-        self._address = address        
+        Input:
+            name (string)    : name of the instrument
+            address (string) : GPIB address
+            reset (bool)     : resets to default values, default=False
+            freq (float)     : initial frequency in Hz, default=1e9
+            pow (float)      : initial power in dBm, default=-143.4
+        '''
+        logging.info(__name__ + ' : Initializing instrument')
+        Instrument.__init__(self, name, tags=['physical'])
+
+        self._address = address
         self._visainstrument = visa.instrument(self._address)
         sleep(1)
 
@@ -77,81 +70,103 @@ class HP8657B(Instrument):
             minval=-143.5, maxval=17, units='dBm')
         self.add_parameter('status', type=types.StringType,
             flags=Instrument.FLAG_SET)
-            
-        # Implement functions    
-        self.add_function('reset')            
-        self.add_function('set_all')
-            
+
+        # Implement functions
+        self.add_function('reset')
+
         # (re)set device to specified values
         if reset:
             self.reset(freq,pow)
         else:
-            self.set_all(freq,pow)
+            logging.warning('instrument does not support getting of values \
+                you need to run set_power and set_frequency manually!')
 
-           
     # initialization related
-    def reset(self, freq, pow):
+    def reset(self, freq=1e9, pow=-143.4):
         '''
         Reset device to default values, has to be ajusted jet
-        
-        Usage:
-        <name>.reset(freq=<float>, pow=<float>)
-        '''        
-        logging.debug(__name__ + ' : Resetting instrument')
-        #  self._visainstrument.write('DC1')
-        self.set_all(100e6,-143.5)
 
-    def set_all(self, freq, pow):
+        Input:
+            freq (float) : Frequency in Hz
+            pow (float)  : Power in dBm
+
+        Output:
+            None
         '''
-        Set frequency and power to specified values
-        
-        Usage:
-        <name>.set_all(freq=<float>, pow=<float>)
-        '''
-        logging.debug(__name__ + ' : set_all function called')        
+        logging.info(__name__ + ' : Resetting instrument')
         self.set_power(pow)
         sleep(0.1)
         self.set_frequency(freq)
-        sleep(0.1) 
+        sleep(0.1)
 
-    #Ccommunication with device
+    #Communication with device
     def _do_set_frequency(self, frequency):
         '''
         Set frequency of device
-        '''	
+
+        Input:
+            freq (float) : Frequency in Hz
+
+        Output:
+            None
+        '''
         logging.debug(__name__ + ' : setting frequency to %s GHz' % frequency)
         # sending value to instrument
         self._visainstrument.write('FR%.0fHZ' % frequency)
- 
+
     def _do_set_power(self,power):
         '''
         Set output power of device
-        '''	
+
+        Input:
+            pow (float) : Power in dBm
+
+        Output:
+            None
+        '''
         logging.debug(__name__ + ' : setting power to %s dBm' % power)
         # sending value to instrument
-        self._visainstrument.write('AP%.1fDM' % power) 
- 
+        self._visainstrument.write('AP%.1fDM' % power)
+
     def _do_set_status(self,status):
         '''
-        Set status to 'on' or 'off'
-        '''	
+        Set status of output channel
+
+        Input:
+            status (string) : 'on' or 'off'
+
+        Output:
+            None
+        '''
+        logging.debug(__name__ + ' : setting status to "%s"' % status)
         if status.upper() == 'ON':
             self._visainstrument.write('R3')
         elif status.upper() == 'OFF':
             self._visainstrument.write('R2')
         else:
             raise ValueError('set_status(): can only set on or off')
-        logging.debug(__name__ + ' : setting status to "%s"' % status)
 
     # shortcuts
     def off(self):
         '''
         Set status to 'off'
+
+        Input:
+            None
+
+        Output:
+            None
         '''
         self.set_status('off')
 
     def on(self):
         '''
         Set status to 'on'
+
+        Input:
+            None
+
+        Output:
+            None
         '''
         self.set_status('on')
