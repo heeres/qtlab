8b69c64e8495a75fecaf5f17b05e624d8325aed1
Reinier Heeres <reinier@heeres.eu>
2008-05-30 16:17:17 +0200

    Basic log routing: everything to qtlab.log, info and higher to console

---
+++ shell_plugins/01_logging.py
@@ -0,0 +1,13 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+    format='%(asctime)s %(name)-12s: %(levelname)-8s %(message)s',
+    datefmt='%m-%d %H:%M',
+    filename='qtlab.log',
+    filemode='a+')
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(name)s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
