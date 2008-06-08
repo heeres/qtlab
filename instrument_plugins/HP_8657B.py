6a6bc48ebcf23dbad8fdc4586cfbe58a55264eed
Reinier Heeres <reinier@heeres.eu>
2008-06-08 20:37:39 +0200

    Fix HP 8656B and 8657A/B issue

--- instrument_plugins/HP_8657B.py
+++ instrument_plugins/HP_8657B.py
@@ -1,6 +1,6 @@
-import HP_8657
+from instrument_plugins._HP_8657 import HP_8657
 
-class HP_8657B(HP_8657.HP_8657):
+class HP_8657B(HP_8657):
 
     def  __init__(self, name, address, reset=False, freq=1e6, pow=None):
-        HP_8657.HP_8657.__init__(self, name, address, '8657B', freq=freq, pow=pow)
+        HP_8657.__init__(self, name, address, '8657B', freq=freq, pow=pow)
