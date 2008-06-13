64c2fd20b1e9a10113ac6b321c213df5b823e2cf
Reinier Heeres <reinier@heeres.eu>
2008-06-13 23:53:19 +0200

    Set default logging level to INFO and add set_debug() function

--- shell_plugins/01_logging.py
+++ shell_plugins/01_logging.py
@@ -1,13 +1,21 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.INFO,
     format='%(asctime)s %(name)-12s: %(levelname)-8s %(message)s',
     datefmt='%m-%d %H:%M',
     filename='qtlab.log',
     filemode='a+')
 
 console = logging.StreamHandler()
-console.setLevel(logging.INFO)
+console.setLevel(logging.WARNING)
 formatter = logging.Formatter('%(name)s: %(levelname)-8s %(message)s')
 console.setFormatter(formatter)
 logging.getLogger('').addHandler(console)
+
+def set_debug(enable):
+    if enable:
+        logging.basicConfig(level=logging.DEBUG)
+        logging.info('Set logging level to DEBUG')
+    else:
+        logging.basicConfig(level=logging.INFO)
+        logging.info('Set logging level to INFO')
