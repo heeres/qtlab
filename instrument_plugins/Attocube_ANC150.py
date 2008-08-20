eaa702e22f0670efd38a7025c6b2e1e40ac88670
Reinier Heeres <reinier@heeres.eu>
2008-08-20 17:04:11 +0200

    Add basic Attocube ANC150 and ARC200 drivers

---
+++ instrument_plugins/Attocube_ANC150.py
@@ -0,0 +1,204 @@
+# Attocube_ANC150, attocube piezo step controller ANC150 driver
+# Reinier Heeres <reinier@heeres.eu>, 2008
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+from instrument import Instrument
+import visa
+import types
+import logging
+import re
+import time
+
+class Attocube_ANC150(Instrument):
+
+    _RE_MODE = re.compile('mode = (\w+)')
+    _RE_FREQ = re.compile('frequency = (\d+) Hz')
+    _RE_VOLT = re.compile('voltage = (\d+) V')
+    _RE_CAP = re.compile('capacitance = (\d+) C')
+    _RE_SN = re.compile('serial number (\d+)')
+
+    def __init__(self, name, address, reset=False, **kwargs):
+        Instrument.__init__(self, name, address=address, reset=False, **kwargs)
+
+        self._address = address
+        self._visa = visa.instrument(self._address,
+                        baud_rate=38400, data_bits=8, stop_bits=1,
+                        parity=visa.no_parity, term_chars='\r\n')
+        self._clear_buffer()
+        self._last_error = ''
+
+        self.add_parameter('version',
+            flags=Instrument.FLAG_GET)
+
+        self.add_parameter('mode',
+            flags=Instrument.FLAG_GETSET,
+            channels=(1, 3),
+            type=types.StringType,
+            format_function=lambda m: self._mode_string(m),
+            doc="mode is one of 'ext', 'stp', 'gnd' or 'cap', or first letter")
+
+        self.add_parameter('frequency',
+            flags=Instrument.FLAG_GETSET,
+            channels=(1, 3),
+            type=types.IntType,
+            minval=0, maxval=8000)
+
+        self.add_parameter('voltage',
+            flags=Instrument.FLAG_GETSET,
+            channels=(1, 3),
+            type=types.IntType,
+            minval=0, maxval=70)
+
+        self.add_parameter('capacitance',
+            flags=Instrument.FLAG_GET,
+            channels=(1, 3),
+            type=types.IntType)
+
+        self.add_function('stepd', parameters=[{
+                'name': 'channel',
+                'type': types.IntType,
+            }, {
+                'name': 'steps',
+                'type': types.IntType
+            }])
+        
+        if reset:
+            self.reset()
+        else:
+            self.get_all()
+
+    def _mode_string(self, m):
+        m = m.upper()
+        if m[0] == 'E':
+            return 'ext'
+        elif m[0] == 'S':
+            return 'stp'
+        elif m[0] == 'G':
+            return 'gnd'
+        elif m[0] == 'C':
+            return 'cap'
+
+    def _clear_buffer(self):
+        self._visa.clear()
+        time.sleep(0.05)
+        self._visa.write('')
+
+    def get_last_error(self):
+        return self._last_error
+
+    def _ask(self, query):
+        self._visa.write(query)
+        try:
+            line, lastline = '', ''
+            while not (line.startswith('OK') or line.startswith('ERROR')):
+                lastline = line
+                line = self._visa.read()
+                print 'Read: %s' % line
+        finally:
+            if line.startswith('ERROR'):
+                self._last_error = line
+                return None
+            else:
+                return lastline
+
+    def _short_cmd(self, query):
+        self._visa.write(query)
+        line = self._visa.read()
+        if line.find('+') != -1:
+            return True
+        else:
+            return False
+
+    def _parse(self, reply, regexp):
+        if reply is None:
+            return None
+
+        m = regexp.match(reply)
+        if m is None:
+            return None
+        else:
+            return m.group(1)
+
+    def reset(self):
+        self._visa.write('resetp')
+
+    def get_all(self):
+        for ch in xrange(1, 4):
+            self.get('mode%d' % ch)
+            self.get('frequency%d' % ch)
+            self.get('voltage%d' % ch)
+
+    def _do_get_version(self):
+        reply = self._ask('ver')
+        sn = self._parse(reply, self._RE_SN)
+        return 'SN %s' % sn
+
+    def _do_get_mode(self, channel):
+        print 'Getting mode'
+        reply = self._ask('getm %d' % channel)
+        return self._parse(reply, self._RE_MODE)
+
+    def _do_set_mode(self, mode, channel):
+        mode = mode.upper()
+        if mode not in ['E', 'S', 'G', 'C', 'EXT', 'STP', 'GND', 'CAP']:
+            logging.warning('Invalid mode: %s', mode)
+            return False
+        mode = mode[0]
+        return self._short_cmd('$M%d%s' % (channel, mode))
+
+    def _do_get_frequency(self, channel):
+        reply = self._ask('getf %d' % channel)
+        return self._parse(reply, self._RE_FREQ)
+
+    def _do_set_frequency(self, freq, channel):
+        reply = self._ask('setf %d %d' % (channel, freq))
+        return (reply is not None)
+
+    def _do_get_voltage(self, channel):
+        print 'Getting voltage'
+        reply = self._ask('getv %d' % channel)
+        return self._parse(reply, self._RE_VOLT)
+
+    def _do_set_voltage(self, volt, channel):
+        reply = self._ask('setv %d %d' % (channel, volt))
+        return (reply is not None)
+
+    def _do_get_capacitance(self, channel):
+        reply = self._ask('getc %d' % channel)
+        return self._parse(reply, self._RE_CAP)
+
+    def stepd(self, channel, steps):
+        if steps == 1:
+            return self._short_cmd('$S%dD' % channel)
+
+        if steps == 'c' or steps == -1:
+            steps = 'c'
+        else:
+            steps = str(steps)
+        reply = self._ask('stepd %d %s' % (channel, steps))
+        return (reply is not None)
+
+    def stepu(self, channel, steps):
+        if steps == 1:
+            return self._short_cmd('$S%dU' % channel)
+
+        if steps == 'c' or steps == -1:
+            steps = 'c'
+        else:
+            steps = str(steps)
+        reply = self._ask('stepu %d %s' % (channel, steps))
+        return (reply is not None)
+
---
+++ instrument_plugins/Attocube_ARC200.py
@@ -0,0 +1,133 @@
+# Attocube_ARC200, attocube resistive readout module ARC200 driver
+# Reinier Heeres <reinier@heeres.eu>, 2008
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+from instrument import Instrument
+import visa
+import types
+import logging
+import re
+import time
+
+class Attocube_ARC200(Instrument):
+
+    REF_5V = 0
+    REF_3V = 1
+    REF_1V = 2
+    REF_05V = 3
+    REF_03V = 4
+    REF_01V = 5
+
+    UNIT_PERCENT = 0
+    UNIT_MICRON = 1
+    UNIT_MM = 2
+    UNIT_V = 3
+    UNIT_MG = 4
+    UNIT_G = 5
+    UNITS = {
+        UNIT_PERCENT: '%',
+        UNIT_MICRON: '',
+        UNIT_MM: 'mm',
+        UNIT_V: 'V',
+        UNIT_MG: 'mS',
+        UNIT_G: 'S'
+    }
+
+    def __init__(self, name, address, reset=False, **kwargs):
+        Instrument.__init__(self, name, address=address, reset=reset, **kwargs)
+
+        self._address = address
+        self._visa = visa.instrument(self._address,
+                        baud_rate=57600, data_bits=8, stop_bits=1,
+                        parity=visa.no_parity, term_chars='')
+
+        self.add_parameter('mode',
+            flags=Instrument.FLAG_SET,
+            type=types.StringType)
+        self.add_parameter('refvoltage',
+            flags=Instrument.FLAG_GETSET,
+            type=types.IntType,
+            minval=0, maxval=self.REF_01V)
+
+        self.add_parameter('units',
+            flags=Instrument.FLAG_SET,
+            type=types.IntType,
+            minval=0, maxval=self.UNIT_G,
+            format_function=lambda uid: self.UNITS[uid])
+
+        self.add_parameter('values',
+            flags=Instrument.FLAG_GET,
+	    format='%.03f, %.03f, %.03f')
+
+        if reset:
+            self.reset()
+        else:
+            self.set_mode(1)
+            self.set_units(self.UNIT_PERCENT)
+            self.get_all()
+
+    def write_line(self, query):
+        self._visa.write(query)
+        time.sleep(0.05)
+        self._visa.write('\r')
+
+    def ask(self, query):
+        self._visa.write(query)
+        time.sleep(0.05)
+        reply = self._visa.read()
+        return reply.rstrip(' \t\r\n')
+        
+    def reset(self):
+        self.write_line('resetp')
+
+    def get_all(self):
+        self.get_values()
+
+    def _do_set_mode(self, mode):
+        '''
+        Set the measurement mode to continuous ('C') or interval ('I')
+        '''
+
+        if type(mode) == types.StringType:
+            if mode.upper() == 'C':
+                mode = 0
+            elif mode.upper() == 'I':
+                mode = 1
+        if int(mode) not in (0, 1):
+            return False
+
+        logging.info('Setting mode %s' % mode)
+                
+        self.write_line('SM%d' % mode)
+        self._visa.clear()
+
+    def _do_set_voltage(self, ref):
+        self.write_line('SRE %d' % ref)
+
+    def _do_get_values(self):
+        reply = self.ask('C')
+        str_list = reply.split(',')
+        float_list = [float(str_item) for str_item in str_list]
+        return float_list
+
+    def _do_set_channel_units(self, channel, units_id):
+        self.write_line('SU%d%d' % (channel, units_id))
+
+    def _do_set_units(self, units_id):
+        self._do_set_channel_units(1, units_id)
+        self._do_set_channel_units(2, units_id)
+        self._do_set_channel_units(3, units_id)
+        self.set_parameter_options('values', units=self.UNITS[units_id])
