dc67ead17a517124a103f688a1eac1705d00b59d
Reinier Heeres <reinier@heeres.eu>
2008-09-18 12:58:58 +0200

    Update QTWindow for pluggable system

--- qtwindow.py
+++ qtwindow.py
@@ -3,11 +3,12 @@ import gobject
 
 from gettext import gettext as _
 
+import qt
 import config
 
 class QTWindow(gtk.Window):
 
-    def __init__(self, title):
+    def __init__(self, title, add_to_main=True):
         gtk.Window.__init__(self, gtk.WINDOW_TOPLEVEL)
 
         self._title = title
@@ -32,6 +33,9 @@ class QTWindow(gtk.Window):
         self.connect('show', lambda x: self._show_hide_cb(x, True))
         self.connect('hide', lambda x: self._show_hide_cb(x, False))
 
+        if add_to_main:
+            self._add_to_main()
+
     def _do_show(self):
         self.show()
 
@@ -45,3 +49,8 @@ class QTWindow(gtk.Window):
             pos = self.get_position()
             self._config.set('%s_pos' % self._title, pos)
 
+    def get_title(self):
+        return self._title
+
+    def _add_to_main(self):
+        qt.mainwin.add_window(self)
