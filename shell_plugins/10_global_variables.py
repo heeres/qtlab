4f28be3a4ecadaaa39ac9a41b56be525ce7702fe
Reinier Heeres <reinier@heeres.eu>
2008-10-16 15:43:14 +0200

    Add QTlab dir to config and path at startup

--- shell_plugins/10_global_variables.py
+++ shell_plugins/10_global_variables.py
@@ -8,6 +8,8 @@ qt.instruments = instruments.get_instruments()
 
 import config
 qt.config = config.get_config()
+qt.config['qtlabdir'] = config.get_workdir()
+sys.path.append(qt.config['qtlabdir'])
 
 from data import Data
 import datalist
