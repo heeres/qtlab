1fe1408559313ed2a2f8860454b80f05e55d4c7f
Reinier Heeres <reinier@heeres.eu>
2008-10-20 16:12:48 +0200

    Use absolute path for lock-file

--- shell_plugins/00_lockfile.py
+++ shell_plugins/00_lockfile.py
@@ -1,7 +1,11 @@
 import os
 import sys
+import config
 
 _remove_lock = True
+
+def get_lockfile():
+    return os.path.join(config.get_workdir(), 'qtlab.lock')
 
 def qtlab_exit():
     print "Closing QTlab..."
@@ -9,13 +13,13 @@ def qtlab_exit():
     global _remove_lock
     if _remove_lock:
         try:
-            os.remove('qtlab.lock')
+            os.remove(get_lockfile())
         except:
             pass
 
 sys.exitfunc = qtlab_exit
 
-if os.path.exists('qtlab.lock'):
+if os.path.exists(get_lockfile()):
     if '-f' not in sys.argv:
         print "QTlab already running, start with '-f' to force start."
         print "Press s<enter> to start anyway or just <enter> to quit."
