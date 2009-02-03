f920fccf82a243fde0aef1545f28e90c4e04dc15
Reinier Heeres <reinier@heeres.eu>
2009-02-03 16:19:03 +0100

    Move imports from shell to qt module.

--- shell/02_qtlab_start.py
+++ shell/02_qtlab_start.py
@@ -12,34 +12,9 @@ except:
     logging.info('psyco acceleration not enabled')
         
 import qt
-
-import qtflow
-qt.flow = qtflow.get_flowcontrol()
-qt.sleep = qt.flow.measurement_idle
-import instruments
-qt.instruments = instruments.get_instruments()
-
-import config
-qt.config = config.get_config()
-qt.config['qtlabdir'] = config.get_workdir()
-
-from data import Data
-qt.data = Data.get_named_list()
-qt.Data = Data
-
-from lib.gui import qtwindow
-qt.windows = qtwindow.QTWindow.get_named_list()
-
-if qt.config.get('plot_type', 'gnuplot') == 'matplotlib':
-    from plot_engines.qtmatplotlib import Plot2D, Plot3D
-else:
-    from plot_engines.qtgnuplot import Plot2D, Plot3D
-
-import plot
-qt.Plot2D = Plot2D
-qt.plots = plot.Plot.get_named_list()
-qt.Plot3D = Plot3D
 from plot import plot
+Plot2D = qt.Plot2D
+Plot3D = qt.Plot3D
 
 # Set exception handler
 try:
--- source/qt.py
+++ source/qt.py
@@ -1,10 +1,24 @@
 # Global namespace
 
-# Variables will be filled in later
-config = None
-data = None
-flow = None
-plots = None
-windows = None
-sleep = None
+from qtflow import get_flowcontrol
+from instruments import get_instruments
+import config as _config
+from data import Data
+from plot import Plot
+from lib.gui.qtwindow import QTWindow
 
+config = _config.get_config()
+config['qtlabdir'] = _config.get_workdir()
+data = Data.get_named_list()
+instruments = get_instruments()
+windows = QTWindow.get_named_list()
+
+flow = get_flowcontrol()
+sleep = flow.measurement_idle()
+
+if config.get('plot_type', 'gnuplot') == 'matplotlib':
+    from plot_engines.qtmatplotlib import Plot2D, Plot3D
+else:
+    from plot_engines.qtgnuplot import Plot2D, Plot3D
+
+plots = Plot.get_named_list()
