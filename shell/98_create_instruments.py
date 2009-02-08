65373de7629770beb9b316df20e7a6d9093c03de
Reinier Heeres <reinier@heeres.eu>
2009-02-08 09:20:34 +0100

    Shell: Clean-up create instruments

--- shell/98_create_instruments.py
+++ shell/98_create_instruments.py
@@ -12,8 +12,3 @@ combined.add_variable_scaled('magnet', dmm1, 'ch1_output', 0.02, -0.13, units='m
 #    'scale': 0.5,
 #    'offset': 0
 #    }], format='%.04f')
-#arc200 = qt.instruments.create('ARC200', 'Attocube_ARC200', address='ASRL5::INSTR')
-#anc150 = qt.instruments.create('ANC150', 'Attocube_ANC150', address='ASRL5::INSTR')
-#pm100 = qt.instruments.create('PM100', 'Thorlabs_PM100', address='ASRL5::INSTR')
-#atto = qt.instruments.create('atto', 'Attocube_Feedback', anc=1, arc=2)
-#xy = qt.instruments.create('XY', 'Dummy_XYStage')
