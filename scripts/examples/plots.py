5a56636752a5298eccebc1c8947f18e448cc1cd6
Reinier Heeres <reinier@heeres.eu>
2009-05-04 14:16:36 +0200

    Add example script for plotting

---
+++ scripts/examples/plots.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+x = np.arange(-10, 10, 0.2)
+y = np.sinc(x)
+p = plot(x, y, 'ks',                    # x vs y with black squares
+        title='sinc(x)',                # Trace title (in legend)
+        legend=True,                    # Show legend
+        plottitle='Plotting example',   # Plot title
+        name='Functions',               # Plot window name
+        clear=True,                     # Clear plot before adding trace
+        xlabel='X value [radians]',     # X label
+        ylabel='f(x) [AU]'              # Y label
+)
+
+# Add another curve to the plot
+x2 = np.arange(-10, 10, 0.1)
+y2 = np.cos(x) / 4 + 0.4
+p.add(x, y2, 'r-', title='cos(x)')
+
+# Adjust x and y range
+p.set_xrange(-11, 11)
+p.set_yrange(-0.3, 1.1)
+
+# Save figure to a file
+p.save_png(filepath='functions.png')
+
