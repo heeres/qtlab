c5040c1cc2efa0ebabe3652dd59fb102bd343f30
Reinier Heeres <reinier@heeres.eu>
2009-05-04 14:39:27 +0200

    Move qtrun/measure1d from shell

--- source/lib/measurement.py
+++ source/lib/measurement.py
@@ -356,10 +356,50 @@ class Measurement(gobject.GObject):
 
         return ret
 
+#FIXME: Change to NamedList
 class Measurements(gobject.GObject):
 
     def __init__(self):
         pass
 
 measurements = None
+
+def measure1d(
+        read_ins, read_var,
+        sweep_ins, sweep_var, start, end, **kwargs):
+
+    m = Measurement()
+    m.add_coordinate(start, end, sweep_ins, sweep_var, **kwargs)
+    m.add_measurement(read_ins, read_var)
+
+def measure2d(
+        read_ins, read_var,
+        xsweep_ins, xsweep_var, xstart, xend,
+        ysweep_ins, ysweep_var, ystart, yend,
+        delay,
+        **kwargs):
+
+    m = Measurement(delay=delay)
+
+    if 'ysteps' in kwargs:
+        m.add_coordinate(ysweep_ins, ysweep_var, ystart, yend,
+            steps=kwargs['ysteps'])
+    elif 'ystepsize' in kwargs:
+        m.add_coordinate(ysweep_ins, ysweep_var, xstart, xend,
+            stepsize=kwargs['ystepsize'])
+    else:
+        print 'measure2d() needs ysteps or ystepsize argument'
+
+    if 'xsteps' in kwargs:
+        m.add_coordinate(xsweep_ins, xsweep_var, xstart, xend,
+            steps=kwargs['xsteps'])
+    elif 'xstepsize' in kwargs:
+        m.add_coordinate(xsweep_ins, xsweep_var, xstart, xend,
+            stepsize=kwargs['xstepsize'])
+    else:
+        print 'measure2d() needs xsteps or xstepsize argument'
+
+    m.add_measurement(read_ins, read_var)
+
+    m.start()
 
