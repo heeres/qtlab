143bff61136c95f9efdd38a72fbcd0640597f077
Reinier Heeres <reinier@heeres.eu>
2010-09-30 21:10:43 +0200

    Add set_return to example_script

--- examples/scripts/example_script.py
+++ examples/scripts/example_script.py
@@ -13,3 +13,6 @@ print 'Arg1: %s, arg2: %s' % (arg1, arg2)
 for key, val in kwargs.iteritems():
     print 'Keyword arg: %s = %s' % (key, val)
 
+# A script can return a value by calling set_return
+set_return('Return value')
+
