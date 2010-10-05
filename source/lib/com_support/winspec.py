9dd60e3c1ce11865530eec974a938949643b3208
Reinier Heeres <reinier@heeres.eu>
2010-10-05 09:06:14 +0200

    Update winspec low level and add instrument

--- source/lib/com_support/winspec.py
+++ source/lib/com_support/winspec.py
@@ -22,6 +22,8 @@ import time
 _exp = None
 _app = None
 _const = None
+_spec_mgr = None
+_spec = None
 
 def _initialize():
     '''
@@ -29,16 +31,65 @@ def _initialize():
     object. Note that it's not possible to request the contents of that;
     one has to look in the Python file generated with gen_py.
     '''
-    global _exp, _app, _const
+    global _exp, _app, _const, _spec_mgr, _spec
     _exp = CreateObject('WinX32.ExpSetup.2')
     _app = CreateObject('WinX32.Winx32App.2')
     _const = Constants(_exp)
+    _spec_mgr = CreateObject('WinX32.SpectroObjMgr')
+    _spec = _spec_mgr.Current
+    _spec.Process(_const.SPTP_INST_LOADCONFIGURATION)
 
 def _get_wavelen(coord, coeffs):
     val = 0
     for power, coef in enumerate(coeffs):
         val += coef * (coord + 1) ** power
     return val
+
+def get_exposure_time():
+    return _exp.SGetParam(_const.EXP_EXPOSURE)[1]
+
+def set_exposure_time(val):
+    return _exp.SetParam(_const.EXP_EXPOSURE, val)
+
+def get_grating():
+    return _spec.GetParam(_const.SPT_CUR_GRATING)[1]
+
+def get_grating_grooves(gr):
+    '''gr is the absolute grating number.'''
+    return _spec.GetParam(_const.SPT_GRAT_GROOVES, gr)[1]
+
+def get_grating_name(gr):
+    '''gr is the absolute grating number.'''
+    return _spec.GetParam(_const.SPT_GRAT_USERNAME, gr)[1]
+
+def get_ngratings():
+    '''Get number of gratings per turret.'''
+    return _spec.GetParam(_const.SPT_GRATINGSPERTURRET)[1]
+
+def get_current_turret():
+    return _spec.GetParam(_const.SPT_ACTIVE_TURRET_NUM)[1]
+
+def set_grating(val):
+    _spec.SetParam(_const.SPT_NEW_GRATING, val)
+    _spec.Move()
+    return get_grating()
+
+def get_temperature():
+    return _exp.SGetParam(_const.EXP_ACTUAL_TEMP)[1]
+
+def get_target_temperature():
+    return _exp.SGetParam(_const.EXP_TEMPERATURE)[1]
+
+def set_target_temperature(val):
+    return _exp.SetParam(_const.EXP_TEMPERATURE, val)
+
+def get_wavelength():
+    return _spec.GetParam(_const.SPT_CUR_POSITION)[1]
+
+def set_wavelength(val):
+    _spec.SetParam(_const.SPT_NEW_POSITION, float(val))
+    _spec.Move()
+    return get_wavelength()
 
 # Default maximum number of sleeps for 250sec
 MAX_SLEEPS = 5000
