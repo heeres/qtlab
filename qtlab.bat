a324d57cb0925111070a7315b0a184e5b7f6412b
Pieter de Groot <pieterdegroot@gmail.com>
2011-09-12 12:31:14 +0200

    qtlab.bat remove version check gnuplot: to fragile

--- qtlab.bat
+++ qtlab.bat
@@ -8,9 +8,7 @@
 :: statements. Comment or uncomment these lines as needed.
 
 :: Add gnuplot to PATH ("binary" folder for >= 4.4.0, "bin" folder for 4.3)
-IF EXIST "%CD%\3rd_party\gnuplot\binary\gnuplot.exe" SET PATH="%CD%\3rd_party\gnuplot\binary;%PATH%"
-
-IF EXIST "%CD%\3rd_party\gnuplot\bin\gnuplot.exe" SET PATH="%CD%\3rd_party\gnuplot\bin;%PATH%"
+SET PATH=%CD%\3rd_party\gnuplot\binary;%PATH%
 
 :: Add Console2 to PATH
 SET PATH=%CD%\3rd_party\Console2\;%PATH%
