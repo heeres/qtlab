935275316a08485acb5894f68f5c62bed3fece8f
Gabriele de Boo <ggdeboo@gmail.com>
2015-04-15 13:02:10 +1000

    Removed unnecessary comments from misc.py. Added requirements for IPython and pyVISA to make it clear which versions are not supported.

--- source/lib/misc.py
+++ source/lib/misc.py
@@ -95,39 +95,9 @@ def usleep(usec):
     while (exact_time() - start) * 1e6 < usec:
         pass
 
-#def get_ipython():
-#    import IPython
-#    if ipython_is_newer((0, 11)):
-#        return IPython.core.ipapi.get()
-#    else:
-#        return IPython.ipapi.get()
-
 def get_traceback():
-#    if ipython_is_newer((0, 11)):
     from IPython.core.ultratb import AutoFormattedTB
-#    else:
-#        from IPython.ultraTB import AutoFormattedTB    
-    # import os
-    # if os.name == 'nt':
-    # #   This line bugs out IPython
-    #     return AutoFormattedTB(color_scheme=u'NoColor')
     return AutoFormattedTB
-
-#def ipython_is_newer(vin):
-#    """
-#    vin is tuple of version (a,b,c) for version "a.b.c"
-#    result gives True for larger or equal version
-#    """
-#    import IPython
-#    vs = IPython.__version__.split('.')
-#    for i in range(len(vs)):
-#        if i > (len(vin)-1):
-#            return True
-#        if int(vs[i]) > vin[i]:
-#            return True
-#        elif int(vs[i]) < vin[i]:
-#            return False
-#    return True
 
 def is_ipython():
     return get_ipython() != None
@@ -135,19 +105,13 @@ def is_ipython():
 def exit_shell():
     if is_ipython():
         ip = get_ipython()
-#        if ipython_is_newer((0, 11)):
         ip.exit() # FIXME This gives annoying request for y/n when called
-#        else:
-#            ip.magic('Exit')
     sys.exit()
 
 def register_exit(func):
     if is_ipython():
         ip = get_ipython()
-#        if ipython_is_newer((0, 11)):
         ip.hooks['shutdown_hook'].add(func, 1)
-#        else:
-#            ip.IP.hooks.shutdown_hook.add(func, 1)
     else:
         import atexit
         atexit.register(func)
